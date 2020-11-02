93a2184265c24ac93cd051422e95fd18b8bd1fcc
imadaqel <emadaqel92@gmail.com>
2020-11-02 20:08:57 +0200

    SEC

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
-var matrix = [];
-for(var i=0; i<3; i++) {
-    matrix[i] = [];
-    for(var j=0; j<3; j++) {
-        matrix[i][j] = undefined;
-    }
-}
+// var matrix = [];
+// for(var i=0; i<3; i++) {
+//     matrix[i] = [];
+//     for(var j=0; j<3; j++) {
+//         matrix[i][j] = undefined;
+//     }
+// }
 
 
-  function loadTableData(items) {
-    const table = document.getElementById("testBody");
+function loadTableData(items) {
+
     var matrix = [];
-for(var i=0; i<3; i++) {
-    matrix[i] = [];
-    for(var j=0; j<3; j++) {
-        matrix[i][j] = undefined;
+    var table = '<table border=2 width=300px height=300px>'
+    for (var i = 0; i < 3; i++) {
+        table += '<tr>'
+        matrix[i] = [];
+        for (var j = 0; j < 3; j++) {
+            table += "<td onClick='changeto()'>"
+            //matrix[i][j] = undefined;
+            uniqueid = `'${i}'+'${j}'`
+            console.log(uniqueid)
+            table += '</td>'
+        }
+        table += '</tr>'
     }
+    table += '</table>'
+
+    console.log(table)
+    document.getElementById("tableoftic").innerHTML = table
+
+
+    //   let row = table.insertRow();
+    //   let date = row.insertCell(0);
+    //   date.innerHTML = undefined;
+    //   let name = row.insertCell(1);
+    //   name.innerHTML = item.name;
+    
+// changeto(){
+//     console.log('id');
+// }
+
 }
 
-      let row = table.insertRow();
-      let date = row.insertCell(0);
-      date.innerHTML = undefined;
-      let name = row.insertCell(1);
-      name.innerHTML = item.name;
-    
-  }
-  loadTableData(items1);
-  loadTableData(items2);
+
+loadTableData()
