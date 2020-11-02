03905dc84f24a4f9257421a5f0872a453652036c
imadaqel <emadaqel92@gmail.com>
2020-11-02 17:54:10 +0200

    first

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
+var matrix = [];
+for(var i=0; i<3; i++) {
+    matrix[i] = [];
+    for(var j=0; j<3; j++) {
+        matrix[i][j] = undefined;
+    }
+}
 
 
+  function loadTableData(items) {
+    const table = document.getElementById("testBody");
+    var matrix = [];
+for(var i=0; i<3; i++) {
+    matrix[i] = [];
+    for(var j=0; j<3; j++) {
+        matrix[i][j] = undefined;
+    }
+}
+
+      let row = table.insertRow();
+      let date = row.insertCell(0);
+      date.innerHTML = undefined;
+      let name = row.insertCell(1);
+      name.innerHTML = item.name;
+    
+  }
+  loadTableData(items1);
+  loadTableData(items2);
